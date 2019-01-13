2db30507af2d949a3e401b43308bf4893d43ee61
alexhsu <ihunghsu@usc.edu>
2019-01-12 20:13:14 -0800

    new data

--- data/get_dic.py
+++ data/get_dic.py
@@ -1,28 +1,30 @@
 #encoding=utf-8
 
-import cPickle as pkl
+import pickle as pkl
 import collections
 import os
 
 
-file_name=['q_train.txt']
+file_name=['raw/q_train.txt']
 count = dict()
 for i in file_name:
     with open(i,'r') as f:
         for line in f.readlines():
             line=line.split()
             for line_per in line:
-                if count.has_key(line_per):
+                if line_per in count.keys():
                     count[line_per]+=1
                 else:
                     count[line_per]=1
 
-count = sorted(count.items(),lambda x,y:cmp(x[1],y[1]), reverse=True)
-print len(count)
+count = sorted(count.items(),key=lambda x:x[1], reverse=True)
+print (len(count))
 count_write = collections.OrderedDict()
-count_write['eos'] = 0
-count_write['UNK'] = 1
-index=2
+count_write['<PAD>']=0
+count_write['<BOS>']=1
+count_write['<EOS>']=2
+count_write['<UNK>']=3
+index=4
 for i in count:
     count_write[i[0]]=index
     index+=1
