8244b5c3edb5be5e9bfcdceb488c7fa16da22649
I-Hung Hsu <ihunghsu@isi.edu>
2019-02-13 18:57:48 -0800

    gitignore

---
+++ regularize_scripts/run_proposed_regularize-b10-g10-d100-z1000-e100.sh
@@ -0,0 +1,4 @@
+source ~/.zshrc
+rm -rf ~/.nv
+cd ~/Code/Text-style-tranfer-with-style-embedding-contraint
+python main.py -m style_regularize -c config/config_proposed_regularize.yaml --test --train --alpha 1 --beta 10 --gamma 10 --delta 100 --zeta 1000 --eta 100
